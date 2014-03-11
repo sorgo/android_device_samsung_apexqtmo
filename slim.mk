030facf2ef004480fec4a6b13ac4810306d648cf
TheREV3N <rev3nt3ch@gmail.com>
2014-03-10 23:03:12 -0700

    slim-ify

--- slim.mk
+++ slim.mk
@@ -6,12 +6,7 @@ $(call inherit-product, vendor/slim/config/gsm.mk)
 # Inherit some common SLIM stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-# Inherit torch settings
-$(call inherit-product, vendor/slim/config/common_ledflash.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=apexqtmo TARGET_DEVICE=apexqtmo BUILD_FINGERPRINT="samsung/apexqtmo/apexqtmo:4.1.2/JZO54K/T699UVBMC5:user/release-keys" PRIVATE_BUILD_DESC="apexqtmo-user 4.1.2 JZO54K T699UVBMC5 release-keys"
-
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=apexqtmo TARGET_DEVICE=apexqtmo BUILD_FINGERPRINT="samsung/apexqtmo/apexqtmo:4.4.2/KVT49L/T699UVBMC5:user/release-keys" PRIVATE_BUILD_DESC="apexqtmo-user 4.4.2 KVT49L T699UVBMC5 release-keys"
 
 PRODUCT_NAME := slim_apexqtmo
 PRODUCT_DEVICE := apexqtmo
-
